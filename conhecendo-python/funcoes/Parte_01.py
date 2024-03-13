93a7a7cbb9906d7b069c26c10c20ac48db4d724c
Hector <hectorlazzari20@gmail.com>
2024-03-13 07:28:38 -0300

    um pouco sobre dicionarios

---
+++ conhecendo-python/funcoes/Parte_01.py
@@ -0,0 +1,5 @@
+'''
+O QUE SÃO FUNÇÕES?
+
+Função é um bloco de código identificado por um nome e pode receber uma lista de parâmetros, esses parâmetros podem ou não ter valores padrões. Usar funções torna o código mais legível e possibilita o reaproveitamento de código. Programar baseado em funções, é o mesmo que dizer que estamos programando de maneira estruturada.
+'''
