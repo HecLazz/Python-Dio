1b7b0a6cec3b89ec8d43c0e0bff006a77c21f1ca
Hector <hectorlazzari20@gmail.com>
2024-03-29 20:09:18 -0300

    classes

---
+++ Orientacao_a_Objetos/classe1.py
@@ -0,0 +1,13 @@
+class Vendedor():
+    def __init__(self, nome):
+        self.nome = nome
+        self.vendas = 0
+
+    def vendeu(self, vendas):
+        self.vendas = vendas
+
+    def bateu_meta(self, meta):
+        if self.vendas > meta:
+            print(self.nome,"Bateu a meta")
+        else:
+            print(self.nome,"Não bateu a meta")
---
+++ Orientacao_a_Objetos/part1.py
@@ -0,0 +1,13 @@
+from classe1 import Vendedor
+
+vendedor1 = Vendedor("Hector")
+v1 = vendedor1
+
+v1.vendeu(1000)
+v1.bateu_meta(500)
+
+vendedor2 = Vendedor("Alex")
+v2 = vendedor2
+
+v2.vendeu(300)
+v2.bateu_meta(600)
